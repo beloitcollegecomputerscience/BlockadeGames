c10411aeab446fc8d073ee7bfc6ea89397ca84a3
KYOH95 <kakasi18@naver.com>
2022-04-25 14:24:28 -0500

    Add new JavaFX project

---
+++ GonuGame/src/application/SceneController.java
@@ -0,0 +1,50 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SceneController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToInGameScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("InGameScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToMainScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToColorScene(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ColorScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void exit(ActionEvent event) {
+		Platform.exit();
+	}
+}
