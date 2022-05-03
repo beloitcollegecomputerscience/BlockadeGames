d50ba2bbff47f138ff666836176ba678ec79eb6f
Jerry2001 <ngohongphuc2001@gmail.com>
2022-05-03 15:29:52 -0400

    Add GameBoard JUnit test

---
+++ src/blockadeGames/GameBoardTest.java
@@ -0,0 +1,20 @@
+package blockadeGames;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameBoardTest {
+
+	@Test
+	public void testConnection(){
+		GameBoard gb = new GameBoard();
+		assertEquals(gb.areThesePositionsConnected(0, 1), false);
+		assertEquals(gb.areThesePositionsConnected(1, 2), true);
+		assertEquals(gb.areThesePositionsConnected(3, 4), true);
+		assertEquals(gb.areThesePositionsConnected(2, 4), true);
+		assertEquals(gb.areThesePositionsConnected(0, 3), false);
+		assertEquals(gb.areThesePositionsConnected(2, 1), true);
+	}
+}
