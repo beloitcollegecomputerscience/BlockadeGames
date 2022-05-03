311f27d63622dd69df10409ebbcfde8aaaddea2a
Jerry2001 <ngohongphuc2001@gmail.com>
2022-05-03 14:49:51 -0400

    Add boardtoString function

--- src/blockadeGames/PlayingPieces.java
+++ src/blockadeGames/PlayingPieces.java
@@ -1,7 +1,7 @@
 package blockadeGames;
 
 public class PlayingPieces {
-	public static int[] boardPosition = {0, 1}; //which piece is black which piece are white
+	public static int[] boardPosition = {0, 0, -1, 1, 1}; //which piece is black which piece are white
 
 	/**
 	 * It checks whether a piece belongs to the current player or not.
@@ -10,5 +10,19 @@ public class PlayingPieces {
 	 */
 	private static boolean isBelongTo(int position) {
 		return (boardPosition[position] == PlayerTurn.getCurrentPlayer());
+	}
+	
+	private static String boardToString() {
+		String board = "";
+		board += boardPosition[0] + "   " + boardPosition[1] + "\n";
+		board += "|\\ /|\n";
+		board += "| " + boardPosition[2] + "|\n";
+		board += "|/ \\|\n";
+		board += boardPosition[3] + "==" + boardPosition[4] + "\n";
+		return board;
+	}
+
+	public static void main(String [] args) {
+		System.out.print(boardToString());
 	}
 }
